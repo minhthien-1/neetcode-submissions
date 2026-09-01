948afc5f67b7f498ee7271ceab89549e647a91ed
Nguyễn Văn Minh Thiện <tnct3000@gmail.com>
2026-09-01 10:56:17 +0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count_s = {}
+        for a in s:
+            if a in count_s:
+                count_s[a] += 1
+            else:
+                count_s[a] = 1
+        count_t = {}
+        for b in t:
+            if b in count_t:
+                count_t[b] += 1
+            else:
+                count_t[b] = 1
+        return count_s == count_t
+        
